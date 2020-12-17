1c3bad8a249a3cdb8461c61b763465a463bcd51d
Taige Wang <taige.wang@aalto.fi>
2020-12-17 06:16:18 +0200

    Finish part1

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [count, setCount] = useState(new Array(props.anecdotes.length).fill(0))
+  const modifyCount = () => {
+    const copy = [...count]
+    copy[selected] += 1
+    setCount(copy)
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{props.anecdotes[selected]}</p>
+      <p>has {count[selected]} votes</p>
+
+      <button onClick={modifyCount}>
+        vote
+      </button>
+      <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>
+        next anecdote
+      </button>
+      <h1>Anecdote with most votes</h1>
+      <p>{props.anecdotes[count.indexOf(Math.max(...count))]}</p>
+    </div>
+
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
