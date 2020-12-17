4a2eeb8933afef9eabefcefdd1fc497225740cac
Taige Wang <taige.wang@aalto.fi>
2020-12-18 01:05:33 +0200

    Finish 2.1-2.5

---
+++ part1/courseinfo/src/components/Course.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Header = (props) => {
+    return (
+      <h2>{props.course}</h2>
+    )
+  }
+  const Part = ({ name, exercises }) => {
+    return (
+      <p>
+        {name} {exercises}
+      </p>
+    )
+  }
+  
+  const Content = ({ parts }) => {
+    return (
+      <>
+        {parts.map(x => <Part key={x.id} name={x.name} exercises={x.exercises} />)}
+      </>
+    )
+  }
+  
+  const Footer = (props) => {
+    return (
+      <p><b>total of {props.sum} exercises</b></p>
+    )
+  }
+  
+  const Course = ({ course }) => {
+    return (
+      <div>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <Footer sum={course.parts.map(x => x.exercises).reduce((a, b) => a + b)} />
+      </div>
+    )
+  }
+  
+  export default Course
