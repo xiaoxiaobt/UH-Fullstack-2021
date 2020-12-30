3e5d710a09473b2f6a5865e2eb31ba03c6fd88bb
Taige Wang <taige.wang@aalto.fi>
2020-12-30 07:10:52 +0200

    Finish 4.13-4.14

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -19,4 +19,26 @@ blogsRouter.post('/', async (request, response) => {
   response.json(savedBlog.toJSON())
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog.toJSON())
+    })
+    .catch(error => next(error))
+})
+
 module.exports = blogsRouter
