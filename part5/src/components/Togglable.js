f5548dc8e1804abb8843ddbc23a47097174c7710
Taige Wang <taige.wang@aalto.fi>
2021-01-07 00:07:37 +0200

    Finish 5.7

--- part5/src/components/Togglable.js
+++ part5/src/components/Togglable.js
@@ -4,7 +4,7 @@ const Togglable = React.forwardRef((props, ref) => {
   const [visible, setVisible] = useState(false)
 
   const hideWhenVisible = { display: visible ? 'none' : '' }
-  const showWhenVisible = { display: visible ? '' : 'none' }
+  const showWhenVisible = { display: visible ? 'inline-block' : 'none' }
 
   const toggleVisibility = () => setVisible(!visible)
 
