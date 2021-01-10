19a908de0d8956f2675c0527e605da082e0d6042
Taige Wang <taige.wang@aalto.fi>
2021-01-10 22:35:57 +0200

    Finish 5.14

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -4,24 +4,45 @@ import { render, fireEvent } from '@testing-library/react'
 //import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
 
-test('renders content', () => {
-  const blog = {
-    title: 'Test Blog',
-    author: 'Blogger',
-    url: 'http://blog.com',
-    likes: 12,
-    user: 'dsfds'
-  }
+const blog = {
+  title: 'Test Blog',
+  author: 'Blogger',
+  url: 'http://blog.com',
+  likes: 12,
+  user: 'temp'
+}
 
-  const component = render(
+let component
+
+beforeEach(() => {
+  component = render(
     <Blog blog={blog} />
   )
+})
 
+test('renders content', () => {
   expect(component.container.querySelector('.titleDiv')).toHaveTextContent(blog.title)
   expect(component.container.querySelector('.titleDiv')).toHaveTextContent(blog.author)
   expect(component.container.querySelector('.titleDiv')).not.toHaveTextContent(blog.likes)
   expect(component.container.querySelector('.titleDiv')).not.toHaveTextContent(blog.url)
-  expect(component.container.querySelector('.titleDiv')).not.toHaveTextContent(blog.user)
+})
+
+test('after clicking the button, children are displayed', () => {
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  const div = component.container.querySelector('.blogContents')
+  expect(div).toHaveTextContent(blog.likes)
+  expect(div).toHaveTextContent(blog.url)
+  expect(div).not.toHaveStyle('display: none')
+})
+
+test('toggled content can be closed', () => {
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  const div = component.container.querySelector('.blogDetails')
+  expect(div).toHaveStyle('display: block')
 })
 
 // test('clicking the button calls event handler once', async () => {
