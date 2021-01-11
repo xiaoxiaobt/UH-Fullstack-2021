2c5bffd84a55315915de3aebd66639870d0dd128
Taige Wang <taige.wang@aalto.fi>
2021-01-12 01:24:54 +0200

    Finish 5.17

--- part5/cypress/integration/blog_app.spec.js
+++ part5/cypress/integration/blog_app.spec.js
@@ -1,10 +1,13 @@
-describe('Blog app', function() {
-  beforeEach(function() {
+describe('Blog app', function () {
+  beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
     cy.visit('http://localhost:3000')
   })
 
-  it('Login form is shown', function() {
-    // ...
+  it('Login form is shown', function () {
+    cy.contains('login').click()
+    cy.contains('username')
+    cy.contains('password')
+    cy.contains('login')
   })
 })
