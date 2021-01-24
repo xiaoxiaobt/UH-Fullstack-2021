abe8ad179afb14c5f8627aefd6682e5fd7762b21
Taige Wang <taige.wang@aalto.fi>
2021-01-24 06:18:07 +0200

    Finish 6.12

--- part6/anecdotes/src/reducers/notificationReducer.js
+++ part6/anecdotes/src/reducers/notificationReducer.js
@@ -1,18 +1,11 @@
-const initialState = ''
-
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
+const reducer = (state = '', action) => {
   switch (action.type) {
     case 'NEW_ANECDOTE_NOTIFICATION':
       return `you added '${action.data.content}'`
     case 'INCREASE_VOTE_NOTIFICATION':
       return `you voted '${action.data.content}'`
-    case 'HIDE_NOTIFICATION':
-      return ``
     default:
-      return initialState
+      return ''
   }
 }
 
