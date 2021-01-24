b2d655d8bea5e520e9390af0fedacc254a8b10a3
Taige Wang <taige.wang@aalto.fi>
2021-01-24 08:40:38 +0200

    Finish 6.20

--- part6/anecdotes/src/components/Filter.js
+++ part6/anecdotes/src/components/Filter.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { filterChange } from '../reducers/filterReducer'
-import { useDispatch } from 'react-redux'
+// import { useDispatch } from 'react-redux'
 
-const Filter = () => {
-  const dispatch = useDispatch()
+const Filter = (props) => {
+  // const dispatch = useDispatch()
+  // const handleChange = (event) => {
+  //   // input-field value is in variable event.target.value
+  //   dispatch(filterChange(event.target.value))
+  // }
+
   const handleChange = (event) => {
     // input-field value is in variable event.target.value
-    dispatch(filterChange(event.target.value))
+    props.filterChange(event.target.value)
   }
   const style = {
     marginBottom: 10
@@ -19,4 +25,15 @@ const Filter = () => {
   )
 }
 
-export default Filter
+const mapDispatchToProps = {
+  filterChange
+}
+
+const ConnectedFilter = connect(
+  null,
+  mapDispatchToProps
+)(Filter)
+
+
+// export default Filter
+export default ConnectedFilter
