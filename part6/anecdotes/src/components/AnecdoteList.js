67976de8e1b8aea86fadf71a78a682d4788e9d54
Taige Wang <taige.wang@aalto.fi>
2021-01-24 08:32:57 +0200

    Finish 6.19

--- part6/anecdotes/src/components/AnecdoteList.js
+++ part6/anecdotes/src/components/AnecdoteList.js
@@ -1,23 +1,28 @@
-
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { connect } from 'react-redux'
+// import { useDispatch, useSelector } from 'react-redux'
 import { increaseVote } from '../reducers/anecdoteReducer'
 import { voteNotification } from '../reducers/notificationReducer'
 
-const AnecdoteList = () => {
-  const anecdotes = useSelector(({ anecdotes, filter }) =>
-    anecdotes.filter(x => x.content.includes(filter))
-  )
-  const dispatch = useDispatch()
+const AnecdoteList = (props) => {
+  // const anecdotes = useSelector(({ anecdotes, filter }) =>
+  //   anecdotes.filter(x => x.content.includes(filter))
+  // )
+  // const dispatch = useDispatch()
 
-  const handleVoteChange = async (anecdote) => {
-    dispatch(increaseVote(anecdote.id))
-    dispatch(voteNotification(anecdote.content, 5000))
+  // const handleVoteChange = async (anecdote) => {
+  //   dispatch(increaseVote(anecdote.id))
+  //   dispatch(voteNotification(anecdote.content, 5000))
+  // }
+
+  const handleVoteChange = (anecdote) => {
+    props.increaseVote(anecdote.id)
+    props.voteNotification(anecdote.content, 5000)
   }
 
   return (
     <>
-      {anecdotes.map(anecdote =>
+      {props.anecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
@@ -33,4 +38,21 @@ const AnecdoteList = () => {
   )
 }
 
-export default AnecdoteList
+const mapStateToProps = (state) => {
+  return {
+    anecdotes: state.anecdotes.filter(x => x.content.includes(state.filter))
+  }
+}
+
+const mapDispatchToProps = {
+  increaseVote,
+  voteNotification
+}
+
+const ConnectedAnecdotes = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AnecdoteList)
+
+// export default AnecdoteList
+export default ConnectedAnecdotes      
