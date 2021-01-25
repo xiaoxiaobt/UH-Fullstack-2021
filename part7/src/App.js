7cfd28ed5e2a47c9a7dc4e953d53dce2dde9ac8f
Taige Wang <taige.wang@aalto.fi>
2021-01-25 20:32:21 +0200

    Finish 7.5

--- part7/src/App.js
+++ part7/src/App.js
@@ -85,6 +85,13 @@ const CreateNew = (props) => {
     })
   }
 
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -102,6 +109,7 @@ const CreateNew = (props) => {
           <input name='info' {...info} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   )
