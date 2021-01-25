1c750d838082d2ee88a32312f4152fbe5d2e0c01
Taige Wang <taige.wang@aalto.fi>
2021-01-25 22:42:37 +0200

    Finish 7.7

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -18,8 +18,15 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  // useEffect()
-
+  useEffect(() => {
+    if (name)
+      axios
+        .get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+        .then(response => setCountry({ data: response.data[0], found: true }))
+        .catch(() => setCountry({ found: false }))
+    else
+      setCountry(null)
+  }, [name])
   return country
 }
 
@@ -40,8 +47,8 @@ const Country = ({ country }) => {
     <div>
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`} />
     </div>
   )
 }
