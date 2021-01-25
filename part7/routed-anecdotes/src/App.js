5a57258858764323a1311fc1beedb5c3d66ce623
Taige Wang <taige.wang@aalto.fi>
2021-01-25 21:14:08 +0200

    Finish 7.6

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -68,12 +68,9 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  // const [content, setContent] = useState('')
-  // const [author, setAuthor] = useState('')
-  // const [info, setInfo] = useState('')
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const { 'reset': resetContent, ...content } = useField('text')
+  const { 'reset': resetAuthor, ...author } = useField('text')
+  const { 'reset': resetInfo, ...info } = useField('text')
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -87,9 +84,9 @@ const CreateNew = (props) => {
 
   const handleReset = (e) => {
     e.preventDefault()
-    content.reset()
-    author.reset()
-    info.reset()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
