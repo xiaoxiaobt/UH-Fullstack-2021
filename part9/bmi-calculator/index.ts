88bb8c74d384096cd5a1245f7487879f91eb33bc
Taige Wang <taige.wang@aalto.fi>
2021-09-05 06:58:14 +0300

    Finish 9.5

--- part9/bmi-calculator/index.ts
+++ part9/bmi-calculator/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import calculateBmi from './bmiCalculator';
 
 const app = express();
 
@@ -6,7 +7,25 @@ app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-const PORT = 3003;
+app.get('/bmi', (req, res) => {
+  const height = String(req.query.height);
+  const weight = String(req.query.weight);
+  try {
+    const bmi = calculateBmi([height, weight])
+    res.send({
+      weight: Number(weight),
+      height: Number(height),
+      bmi
+    });
+  }
+  catch (e) {
+    res.send({
+      error: "malformatted parameters"
+    });
+  }
+});
+
+const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
