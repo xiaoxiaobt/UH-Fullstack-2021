dffc2accfd02fdd2811e1521d21f4d51f3bcf5c5
Taige Wang <taige.wang@aalto.fi>
2021-09-06 01:27:52 +0300

    Finish 9.6

--- part9/bmi-calculator/bmiCalculator.ts
+++ part9/bmi-calculator/bmiCalculator.ts
@@ -1,30 +1,30 @@
-const calculateBmi = (args: Array<String>): String => {
-  if (args.length < 2) throw new Error('Not enough arguments')
-  if (args.length > 2) throw new Error('Too many arguments')
+const calculateBmi = (args: Array<string>): string => {
+  if (args.length < 2) throw new Error('Not enough arguments');
+  if (args.length > 2) throw new Error('Too many arguments');
   const [height, weight] = args.map(Number);
 
   if (isNaN(height) || isNaN(weight)) throw new Error('Provided values were not numbers!');
-  else if (height <= 0 || weight <= 0) throw new Error("Non-positive values are not allowed")
+  else if (height <= 0 || weight <= 0) throw new Error("Non-positive values are not allowed");
 
-  const bmi: number = 10000 * weight / (height * height)
+  const bmi: number = 10000 * weight / (height * height);
   if (bmi < 16)
-    return "Underweight (Severe thinness)"
+    return "Underweight (Severe thinness)";
   else if (bmi < 17)
-    return "Underweight (Moderate thinness)"
+    return "Underweight (Moderate thinness)";
   else if (bmi < 18.5)
-    return "Underweight (Mild thinness)"
+    return "Underweight (Mild thinness)";
   else if (bmi < 25)
-    return "Normal range"
+    return "Normal range";
   else if (bmi < 30)
-    return "Overweight (Pre-obese)"
+    return "Overweight (Pre-obese)";
   else if (bmi < 35)
-    return "Obese (Class I)"
+    return "Obese (Class I)";
   else if (bmi < 40)
-    return "Obese (Class II)"
+    return "Obese (Class II)";
   else
-    return "Obese (Class III)"
-}
+    return "Obese (Class III)";
+};
 
 // console.log(calculateBmi(process.argv))
 
-export default calculateBmi
+export default calculateBmi;
--- part9/bmi-calculator/exerciseCalculator.ts
+++ part9/bmi-calculator/exerciseCalculator.ts
@@ -1,6 +1,6 @@
-type RatingDescription = 'need improvements' | 'not too bad but could be better' | 'excellent'
-const ratingDescriptions: Array<RatingDescription> = ['need improvements', 'not too bad but could be better', 'excellent']
-type Rating = 1 | 2 | 3
+type RatingDescription = 'need improvements' | 'not too bad but could be better' | 'excellent';
+const ratingDescriptions: Array<RatingDescription> = ['need improvements', 'not too bad but could be better', 'excellent'];
+type Rating = 1 | 2 | 3;
 
 interface Result {
   periodLength: number
@@ -13,26 +13,26 @@ interface Result {
 }
 
 const calculateExercises = (args: Array<string>): Result => {
-  const argumentList: Array<number> = args.map(Number).slice(2)
+  const argumentList: Array<number> = args.map(Number).slice(2);
   if (argumentList.length <= 1) {
-    throw new Error('Not enough arguments')
+    throw new Error('Not enough arguments');
   } else if (argumentList.some(n => isNaN(n))) {
-    throw new Error('All values should be numbers')
+    throw new Error('All values should be numbers');
   } else if (argumentList.some(n => n < 0)) {
-    throw new Error('Negative numbers are not allowed')
+    throw new Error('Negative numbers are not allowed');
   }
 
-  const record = argumentList.slice(1)
-  const target = argumentList[0]
-  const periodLength: number = record.length
-  const trainingDays: number = record.filter(x => x > 0).length
-  const average: number = record.reduce((a, b) => a + b) / periodLength
-  const success: boolean = average >= target
-  const rating: Rating = average >= target ? 3 : (average >= target * 0.5 ? 2 : 1)
-  const ratingDescription: RatingDescription = ratingDescriptions[rating - 1]
+  const record = argumentList.slice(1);
+  const target = argumentList[0];
+  const periodLength: number = record.length;
+  const trainingDays: number = record.filter(x => x > 0).length;
+  const average: number = record.reduce((a, b) => a + b) / periodLength;
+  const success: boolean = average >= target;
+  const rating: Rating = average >= target ? 3 : (average >= target * 0.5 ? 2 : 1);
+  const ratingDescription: RatingDescription = ratingDescriptions[rating - 1];
   return {
     periodLength, trainingDays, success, rating, ratingDescription, target, average
-  }
-}
+  };
+};
 
-console.log(calculateExercises(process.argv))
+console.log(calculateExercises(process.argv));
