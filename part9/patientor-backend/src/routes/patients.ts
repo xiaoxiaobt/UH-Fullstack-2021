f344015fa818925d62cb0b06cc342616ebf147f1
Taige Wang <taige.wang@nokia.com>
2022-06-01 11:44:47 +0300

    Finish 9.12-9.13

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,10 +1,24 @@
 import express from 'express';
 import patientService from '../services/patientService';
+import { NewPatient } from '../types';
+import toNewPatient from '../utils';
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
   res.json(patientService.getPatients());
+});
+
+router.post('/', (req, res) => {
+  try {
+    const newPatient: NewPatient = toNewPatient(req.body);
+
+    const addedEntry = patientService.addPatient(newPatient);
+    res.json(addedEntry);
+  } catch (e) {
+    const err = e as Error;
+    res.status(400).send(err.message);
+  }
 });
 
 export default router;
---
+++ part9/patientor-backend/src/utils.ts
@@ -0,0 +1,81 @@
+import { Gender, NewPatient } from "./types";
+
+const isString = (text: unknown): text is string => {
+  return typeof text === 'string' || text instanceof String;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isGender = (param: any): param is Gender => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  return Object.values(Gender).includes(param);
+};
+
+const parseName = (name: unknown): string => {
+  if (!name || !isString(name)) {
+    throw new Error('Incorrect or missing name: ' + name);
+  }
+  return name;
+};
+
+const parseSSN = (ssn: unknown): string => {
+  if (!ssn || !isString(ssn) || ssn.length !== 11) {
+    throw new Error('Incorrect or missing SSN: ' + ssn);
+  }
+  return ssn;
+};
+
+const parseGender = (gender: unknown): Gender => {
+  if (!gender || !isGender(gender)) {
+    throw new Error('Incorrect or missing gender: ' + gender);
+  }
+  return gender;
+};
+
+const parseOccupation = (occupation: unknown): string => {
+  if (!occupation || !isString(occupation)) {
+    throw new Error('Incorrect or missing occupation: ' + occupation);
+  }
+  return occupation;
+};
+
+
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date));
+};
+
+const parseDate = (date: unknown): string => {
+  if (!date || !isString(date) || !isDate(date)) {
+    throw new Error('Incorrect or missing date: ' + date);
+  }
+  return date;
+};
+
+
+type PatientFields = {
+  name: string,
+  dateOfBirth: string,
+  ssn: string,
+  gender: Gender,
+  occupation: string
+};
+
+const toNewPatient = ({
+  name,
+  dateOfBirth,
+  ssn,
+  gender,
+  occupation
+}: PatientFields): NewPatient => {
+
+  const newEntry: NewPatient = {
+    name: parseName(name),
+    dateOfBirth: parseDate(dateOfBirth),
+    ssn: parseSSN(ssn),
+    gender: parseGender(gender),
+    occupation: parseOccupation(occupation)
+  };
+
+  return newEntry;
+};
+
+export default toNewPatient;
