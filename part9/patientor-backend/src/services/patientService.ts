e643b43c5495ca186a8048739f1de21355b8e43f
Taige Wang <taige.wang@nokia.com>
2022-06-01 11:44:47 +0300

    Finish 9.12-9.13

--- part9/patientor-backend/src/services/patientService.ts
+++ part9/patientor-backend/src/services/patientService.ts
@@ -1,11 +1,23 @@
-import patients from '../../data/patients.json';
-import { NonSensitivePatient } from '../types';
+import patientsData from '../../data/patients.json';
+import { NewPatient, NonSensitivePatient, Patient } from '../types';
+import { v1 as uuid } from 'uuid';
+
+const patients: Array<Patient> = patientsData as Array<Patient>;
 
 const getPatients = (): Array<NonSensitivePatient> => {
   return patients.map(
     ({ id, name, dateOfBirth, gender, occupation }) =>
-      ({ id, name, dateOfBirth, gender, occupation })
+      ({ id, name, dateOfBirth, gender, occupation }) as NonSensitivePatient
   );
 };
 
-export default { getPatients };
+const addPatient = (entry: NewPatient): Patient => {
+  const newPatientEntry: Patient = {
+    id: uuid(),
+    ...entry
+  };
+  patients.push(newPatientEntry);
+  return newPatientEntry;
+};
+
+export default { getPatients, addPatient };
