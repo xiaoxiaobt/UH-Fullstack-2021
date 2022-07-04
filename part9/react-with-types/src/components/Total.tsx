47cba556d3911fb00090f0575b31353d4f1edcb8
Taige Wang <taige.wang@nokia.com>
2022-07-04 17:19:35 +0300

    Finish 9.14

---
+++ part9/react-with-types/src/components/Total.tsx
@@ -0,0 +1,10 @@
+const Total = ({ courseParts }: { courseParts: { name: string, exerciseCount: number }[] }) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+    </p>
+  )
+}
+
+export default Total;
