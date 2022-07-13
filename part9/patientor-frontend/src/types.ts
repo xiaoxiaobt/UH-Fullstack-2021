95410341e953d1e3324bb14205789e34aefe3f22
Taige Wang <taige.wang@aalto.fi>
2022-07-13 17:11:38 +0300

    Finish 9.20-9.21

--- part9/patientor-frontend/src/types.ts
+++ part9/patientor-frontend/src/types.ts
@@ -1,4 +1,4 @@
-export interface Diagnosis {
+export interface Diagnose {
   code: string;
   name: string;
   latin?: string;
@@ -17,4 +17,47 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare',
+  employerName: string,
+  sickLeave?: {
+    startDate: string,
+    endDate: string
+  }
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: 'Hospital',
+  discharge: {
+    date: string;
+    criteria: string;
+  }
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
