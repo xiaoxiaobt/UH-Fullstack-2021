82d5b1675fad6f7ac98e074428326a3b46d24746
Taige Wang <taige.wang@aalto.fi>
2022-07-15 21:11:29 +0300

    Cleanup unused code

--- part9/patientor-frontend/src/AddPatientModal/FormField.tsx
+++ part9/patientor-frontend/src/AddPatientModal/FormField.tsx
@@ -91,49 +91,11 @@ export const NumberField = ({ field, label, min, max }: NumberProps) => {
           if (value > max) setValue(max);
           else if (value <= min) setValue(min);
           else setValue(Math.floor(value));
-      }}
+        }}
       />
       <Typography variant="subtitle2" style={{ color: "red" }}>
         <ErrorMessage name={field.name} />
       </Typography>
     </div>
-  );
-};
-
-export const DiagnosisSelection = ({
-  diagnoses,
-  setFieldValue,
-  setFieldTouched,
-}: {
-  diagnoses: Diagnose[];
-  setFieldValue: FormikProps<{ diagnosisCodes: string[] }>["setFieldValue"];
-  setFieldTouched: FormikProps<{ diagnosisCodes: string[] }>["setFieldTouched"];
-}) => {
-  const [selectedDiagnoses, setDiagnoses] = useState<string[]>([]);
-  const field = "diagnosisCodes";
-  const onChange = (data: string[]) => {    
-    setDiagnoses([...data]);
-    setFieldTouched(field, true);
-    setFieldValue(field, selectedDiagnoses);
-  };
-
-  const stateOptions = diagnoses.map((diagnosis) => ({
-    key: diagnosis.code,
-    text: `${diagnosis.name} (${diagnosis.code})`,
-    value: diagnosis.code,
-  }));
-
-  return (
-    <FormControl style={{ width: 552, marginBottom: '30px' }}>
-      <InputLabel>Diagnoses</InputLabel>
-      <Select multiple value={selectedDiagnoses} onChange={(e) => onChange(e.target.value as string[])} input={<Input />}>
-        {stateOptions.map((option) => (
-          <MenuItem key={option.key} value={option.value}>
-            {option.text}
-          </MenuItem>
-        ))}
-      </Select>
-      <ErrorMessage name={field} />
-    </FormControl>
   );
 };
